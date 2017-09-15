75fb35a846bb98e68c54decfe3444b32bb079091
Luis Hidalgo de Tena <luishidalgodetena@gmail.com>
2017-09-15 12:49:35 +0200

    refactoring code structure

--- frontEnd/controllers/ctrIndex.js
+++ frontEnd/controllers/ctrIndex.js
@@ -1,7 +1,15 @@
 // controller.js = Luis Hidalgo de Tena
-var app = new Vue({
-    el : '#app',
-    data :{
-        name : "Luis Hidalgo de Tena"
+var app = new Vue(
+    {
+        el : '#intro',
+        data :
+        {
+            user :
+            {
+                name : "Luis Hidalgo de Tena"
+                email: "luishidalgodetena@gmail.com"
+            }
+        }
     }
-})
+    )
+findMe
